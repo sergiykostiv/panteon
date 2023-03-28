5c749eb25bb6285ac77249b745dc59d9b89de9d5
sergiy kostiv <sergiy,kostiv@gmail.com>
2023-03-28 22:30:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider-trainers').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true
+  });
+
+  $('.slider-room').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true
+  });
+});
